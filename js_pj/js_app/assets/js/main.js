c420c788c38a9d6102534f1901809cedf6d2c0d7
sadamoriwaco <97593024+sadamoriwaco@users.noreply.github.com>
2022-12-14 15:59:11 +0900

    callback関数の詳細について要確認

--- js_pj/js_app/assets/js/main.js
+++ js_pj/js_app/assets/js/main.js
@@ -1,21 +1,52 @@
-const child = document.querySelector('.child');
-const cb = function(entries, observer){
-    entries.forEach(entry =>{
-        if(entry.isIntersecting){
-            console.log('inview');
-            entry.target.classList.add('inview');
-        }else{
-            console.log('out view');
-            entry.target.classList.remove('inview');
+document.addEventListener('DOMContentLoaded', function(){
+
+    const child = document.querySelector('.child');
+    const cb = function(entries, observer){
+        entries.forEach(entry =>{
+            if(entry.isIntersecting){
+                console.log('inview');
+                entry.target.classList.add('inview');
+            }else{
+                console.log('out view');
+                entry.target.classList.remove('inview');
+            }
+        });
+    }
+    
+    const so = new IntersectionObserver(callback.bind(this),options)
+    const options = {
+        root:null,
+        rootMargin:"-300px 0px",
+        threshold:[0,0.5,1]
+    };
+    
+    const io = new IntersectionObserver(cb,options);
+    // io.observe(child);
+    els.forEach(el => io.observer(el));
+});
+
+class ScrollObserver{
+    constructor(els){
+        this.els = document.querySelectorAll(els);
+        const defaultOptions = {
+            root:null,
+            rootMargin:"0px",
+            threshold:0
+            
         }
-    });
+    }
 }
 
-const options = {
-    root:null,
-    rootMargin:"-300px 0px",
-    threshold:[0,0.5,1]
-};
+_init(){
+    const callback = function(entries,options){
+        entries.forEach(entry =>{
+            if(entry.isIntersecting){
+                this.cb(entry.target,false)
+            }
+        })
+    }
+}
 
-const io = new IntersectionObserver(cb,options);
-io.observe(child);
+destroy(){
+    this.io.disconnect();
+}
